c1c3cc1d3bd5559ce1bfbba3ec76c883fae64afd
legerd <vasho92s7333@gmail.com>
2022-11-26 08:58:48 +0900

    add keybinding

--- app/app.py
+++ app/app.py
@@ -42,7 +42,7 @@ def show_list():
     generation_type= request.args.get("type", type=str, default="t2i")
     s_zero=time.time()
     page= request.args.get("page", type=int, default=1)
-    limit = 100
+    limit = 10
     if generation_type=="t2i":
         img_src_list=glob.glob(OUTPUTS_DIR+"stable-diffusion-webui/outputs/txt2img-images/*.png")
     else:
@@ -52,7 +52,17 @@ def show_list():
     img_src_list_page=img_src_list[(page - 1)*limit: page*limit]
     ln=len(img_src_list)
     # prompt_info_list_page=[get_pnginfo(img_src.replace("/static/","/mnt/vol_b/")) for img_src in img_src_list_page]
-    pagination = Pagination(page=page, per_page=limit,page_parameter="page", total=ln, css_framework='bootstrap5')
+    pagination = Pagination(page=page, per_page=limit,page_parameter="page",css_framework='',inner_window=1,outer_window=0,link_size=0.7, total=ln,)
+    my_pagination_links=["/"]
+    # max_page=
+    # if page==1:
+    #     my_pagination_links.append("")
+    # else:
+    #     my_pagination_links.append("/?page="+(page-1))
+    # if page==:
+    #     my_pagination_links.append("")
+    # else:
+    #     my_pagination_links.append("/?page="+(page-1))
     leng=time.time()-s_zero
     # meta=jsonify({"meta":"jj"});
     meta=pnginfo_from_path(img_src_list_page[0].replace(STATIC_DIR,OUTPUTS_DIR))
@@ -65,12 +75,22 @@ def get_pnginfo():
     path=request.args.get("path", type=str, default="").replace(STATIC_DIR,OUTPUTS_DIR)
     data={"meta":pnginfo_from_path(path)}
     return jsonify(data)
+
+@app.route("/delimg", methods=['POST'])
+def del_img():
+    path=request.json["path"].replace(STATIC_DIR,OUTPUTS_DIR)
+    if os.path.exists(path):
+        os.remove(path)
+        data={"status":"ok"}
+    else:
+        data={"status":"not found"}
+    return jsonify(data)
 @app.route("/album")
 def show_album():
     generation_type= request.args.get("type", type=str, default="t2i")
     s_zero=time.time()
     page= request.args.get("page", type=int, default=1)
-    limit = 10
+    limit = 12
     if generation_type=="t2i":
         img_src_list=glob.glob(OUTPUTS_DIR+"stable-diffusion-webui/outputs/txt2img-images/*.png")
     else:
