16728b0312d9aa5482aaee264aaf3362926a2eba
legerd <vasho92s7333@gmail.com>
2022-11-28 01:41:21 +0900

    add path option

--- app/app.py
+++ app/app.py
@@ -11,6 +11,9 @@ import warnings
 from numpy import inf
 from pyngrok import ngrok
 import app.config as config
+from urllib.parse import quote
+from urllib.parse import unquote
+
 
 OUTPUTS_DIR=config.OUTPUTS_DIR
 STATIC_DIR=config.STATIC_DIR
@@ -25,7 +28,12 @@ def pnginfo_from_path(path):
         return str(text)
     else:
         return "this image hasn't parametas"
-
+def get_img_id_from_path(path):
+    try:
+        img_id=int(os.path.basename(path)[:5])
+    except:
+        img_id=inf
+    return img_id
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -37,18 +45,20 @@ tunnels = ngrok.get_tunnels()
 print("\n\033[32;1m***NGROK PUBLIC URL***:\033[34m",tunnels[0].public_url,"\033[0m")
 
 
+# @app.route("/",defaults={'path': quote("stable-diffusion-webui/outputs/txt2img-images", safe='')})
 @app.route("/")
+@app.route("/list")
 def show_list():
+    path=quote("stable-diffusion-webui/outputs/txt2img-images", safe='')
+    path=request.args.get("path", type=str, default=path)
+    path=unquote(path)
     generation_type= request.args.get("type", type=str, default="t2i")
     s_zero=time.time()
     page= request.args.get("page", type=int, default=1)
     limit= request.args.get("limit", type=int, default=100)
     
     # limit = 10
-    if generation_type=="t2i":
-        img_src_list=glob.glob(OUTPUTS_DIR+"stable-diffusion-webui/outputs/txt2img-images/*.png")
-    else:
-        img_src_list=(OUTPUTS_DIR+"stable-diffusion-webui/outputs/img2img-images/*.png")
+    img_src_list=glob.glob(OUTPUTS_DIR+path+"/*.png")
     img_src_list=[img_src.replace(OUTPUTS_DIR,STATIC_DIR) for img_src in img_src_list]
     img_src_list=sorted(img_src_list,key=get_img_id_from_path, reverse=True)
     img_src_list_page=img_src_list[(page - 1)*limit: page*limit]
@@ -71,7 +81,7 @@ def show_list():
     # meta=jsonify({"meta":"jj"});
     meta=pnginfo_from_path(img_src_list_page[0].replace(STATIC_DIR,OUTPUTS_DIR))
     return render_template("list.html",
-        pagination=pagination,img_src_list=img_src_list_page,data=leng,meta=meta
+        pagination=pagination,img_src_list=img_src_list_page,data=leng,meta=meta,path=path,
     )
 
 @app.route("/pnginfo")
@@ -89,17 +99,22 @@ def del_img():
     else:
         data={"status":"not found"}
     return jsonify(data)
+# @app.route("/album",defaults={'path':quote("stable-diffusion-webui/outputs/txt2img-images", safe='')})
 @app.route("/album")
 def show_album():
-    generation_type= request.args.get("type", type=str, default="t2i")
+    # path=unquote(path)
+    path=quote("stable-diffusion-webui/outputs/txt2img-images", safe='')
+    path=request.args.get("path", type=str, default=path)
+    path=unquote(path)
+    # path="stable-diffusion-webui/outputs/img2img-images"
+    # generation_type= request.args.get("type", type=str, default="t2i")
     limit= request.args.get("limit", type=int, default=100)
     s_zero=time.time()
     page= request.args.get("page", type=int, default=1)
     limit = 12
-    if generation_type=="t2i":
-        img_src_list=glob.glob(OUTPUTS_DIR+"stable-diffusion-webui/outputs/txt2img-images/*.png")
-    else:
-        img_src_list=(OUTPUTS_DIR+"stable-diffusion-webui/outputs/img2img-images/*.png")
+    # if generation_type=="t2i":
+    #     img_src_list=glob.glob(OUTPUTS_DIR+path+"/*.png")
+    img_src_list=glob.glob(OUTPUTS_DIR+path+"/*.png")
     img_src_list=[img_src.replace(OUTPUTS_DIR,STATIC_DIR) for img_src in img_src_list]
     img_src_list=sorted(img_src_list,key=get_img_id_from_path, reverse=True)
     img_src_list_page=img_src_list[(page - 1)*limit: page*limit]
@@ -108,15 +123,10 @@ def show_album():
     pagination = Pagination(page=page, per_page=limit,page_parameter="page", total=ln, css_framework='bootstrap5')
     leng=time.time()-s_zero
     return render_template("history.html",
-        pagination=pagination,img_src_list=img_src_list_page,data=leng
+        pagination=pagination,img_src_list=img_src_list_page,data=leng,path=path,
     )
 
-def get_img_id_from_path(path):
-    try:
-        img_id=int(os.path.basename(path)[:5])
-    except:
-        img_id=inf
-    return img_id
+
 #おまじない
 if __name__ == "__main__":
     app.run()
